1eedd2dc1b42d25002ab4833d4de8be993a0b270
Marco-Tranaso <marco.tranaso@edu-its.it>
2025-02-19 12:26:16 +0100

    add es3 js

---
+++ js/tranaso-marco-units-01-04/03-functions/09-merger/main.js
@@ -0,0 +1,15 @@
+function merger(a, b) {
+    if (typeof a === "number" && typeof b === "number") {
+        return a + b;
+    } 
+    if (typeof a === "string" && typeof b === "string") {
+        return a + b;
+    }
+    return null;
+}
+console.log(merger(5, 10));        
+console.log(merger("Hello, ", "World!"));  
+console.log(merger(3, "text"));    
+console.log(merger("123", "456")); 
+console.log(merger(true, false));  
+console.log(merger(7.5, 2.5));        
