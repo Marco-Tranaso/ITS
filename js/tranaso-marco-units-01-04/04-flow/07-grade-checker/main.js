d47eae4a9f91f561906c3ff05b94e059068c76e1
Marco-Tranaso <marco.tranaso@edu-its.it>
2025-02-27 12:28:49 +0100

    add es7 js

--- js/tranaso-marco-units-01-04/04-flow/07-grade-checker/main.js
+++ js/tranaso-marco-units-01-04/04-flow/07-grade-checker/main.js
@@ -1,18 +1,26 @@
-for (i=60;i<=100<i++;){
+function assignGrade(score){
     let message;
-    switch (language){
-        case 'en':
-        message = 'Hello World';
-        break;
-        case 'de':
-        message = 'Hallo Welt';
-        break;
-        case 'fr':
-        message = 'Bonjour le monde';
-        break;
+    switch (score){
+        case 10:
+            message = 'A';
+            break;
+        case 9:
+            message = 'B';
+            break;
+        case 8:
+            message = 'C';
+            break;
+        case 7:
+            message = 'D';
+            break;
+        case 6:
+            message = 'E';
+            break;
     }
+    return message;
+}
 
-    for (i=60;i<=language<i++;){
-        console.log(language)
-    }
+for (let i = 60; i <= 100; i++) {
+    let grade = assignGrade(Math.floor(i / 10));
+    console.log(`For ${i}, you got a ${grade}.`);
 }
